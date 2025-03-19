91570c0c30408162e2fac20cb52a1086779ae681
Meeran <151153653+Meeran1998@users.noreply.github.com>
2025-03-19 14:50:20 +0530

    changed one to 1

--- javascript/array_methods.js
+++ javascript/array_methods.js
@@ -28,7 +28,7 @@
 // console.log(a.sort())
 // console.log(a.reverse())
 
-let a = ["nokia","motorola","blackberry","samsung","vivo","oneplus"]
+let a = ["nokia","motorola","blackberry","samsung","vivo","1plus"]
 // console.log(a.slice(3,3))
 // console.log(a)
 let b =[10,20,30,40,50,60]
